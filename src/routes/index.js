545f3a36fcaea6943b546cc163897c6eb6df96fe
Loice-vanessa <icyezavanessa@gmail.com>
2022-11-07 11:12:49 +0200

    Initialize project using Create React App

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {} from "react-router-dom"
+
+const MainRoutes =()=>{
+    return(
+        <Routes>
+            <Route path="/login" element={}/>
+            <Route path="/login" element={}/>
+            <Route path="/login" element={}/>
+
+        </Routes>
+    )
+}
