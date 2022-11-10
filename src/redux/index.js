75e9b0667876040c1669137b0bb83c9de6859abb
Loice-vanessa <icyezavanessa@gmail.com>
2022-11-10 09:24:05 +0200

    get all prodructs

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./auth";
-
+import product from "./product";
 const store = configureStore({
   reducer: {
-    auth:auth,
-
+    auth: auth,
+    product: product,
   },
 });
+
 export default store;
